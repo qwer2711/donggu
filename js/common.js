a50c8dbf45f703e246976a8ae70269f5b505daf7
qwer2711 <pjy940222@gmail.com>
2023-08-16 15:57:44 +0900

    3

--- js/common.js
+++ js/common.js
@@ -4,6 +4,117 @@
  * 업데이트 : 2023-08-03
  ******************************************************** */
 window.addEventListener("load", function () {
+  function programSlider() {
+    $("#programList").slick({
+      autoplay: true,
+      autoplaySpeed: 1800,
+      speed: 600,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      pauseOnHover: true,
+      dots: false,
+      pauseOnDotsHover: false,
+      cssEase: "linear",
+      // fade:true,
+      draggable: true,
+      prevArrow:
+        '<a class="slick-prev" href="#"><i class="fa-solid fa-chevron-left"></i></a>',
+      nextArrow:
+        '<a class="slick-next" href="#"><i class="fa-solid fa-chevron-right"></i></a>',
+      responsive: [
+        // 반응형 웹 구현 옵션
+        {
+          breakpoint: 1441, //화면 사이즈 960px
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 830,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 481,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+    var progSlider = $("#programList");
+    var slickOptions = {
+      autoplay: true,
+      autoplaySpeed: 1800,
+      speed: 600,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      pauseOnHover: true,
+      dots: false,
+      pauseOnDotsHover: false,
+      cssEase: "linear",
+      // fade:true,
+      draggable: true,
+      prevArrow:
+        '<a class="slick-prev" href="#"><i class="fa-solid fa-chevron-left"></i></a>',
+      nextArrow:
+        '<a class="slick-next" href="#"><i class="fa-solid fa-chevron-right"></i></a>',
+      responsive: [
+        // 반응형 웹 구현 옵션
+        {
+          breakpoint: 1441, //화면 사이즈 960px
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 830,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 481,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 767,
+          settings: "unslick",
+        },
+      ],
+    };
+    if ($(window).width() < 767) {
+      progSlider.slick("unslick");
+    } else {
+      progSlider.not(".slick-initialized").slick(slickOptions);
+    }
+    $(window).on("resize", function () {
+      if ($(window).width() < 767) {
+        progSlider.slick("unslick");
+      } else {
+        progSlider.not(".slick-initialized").slick(slickOptions);
+      }
+    });
+  }
+  programSlider();
+
   // js
   function setScreenSize() {
     let vh = window.innerHeight * 0.01;
@@ -29,59 +140,6 @@ window.addEventListener("load", function () {
       '<a class="slick-prev" href="#"><i class="fa-solid fa-chevron-left"></i></a>',
     nextArrow:
       '<a class="slick-next" href="#"><i class="fa-solid fa-chevron-right"></i></a>',
-  });
-  var progSlider = $("#programList");
-  $(window).on("load resize", function () {
-    if ($(window).width() < 767) {
-      progSlider.slick("unslick");
-    } else {
-      progSlider.not(".slick-initialized").slick(slickOptions);
-    }
-  });
-  $("#programList").slick({
-    autoplay: true,
-    autoplaySpeed: 1800,
-    speed: 600,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    pauseOnHover: true,
-    dots: false,
-    pauseOnDotsHover: false,
-    cssEase: "linear",
-    // fade:true,
-    draggable: true,
-    prevArrow:
-      '<a class="slick-prev" href="#"><i class="fa-solid fa-chevron-left"></i></a>',
-    nextArrow:
-      '<a class="slick-next" href="#"><i class="fa-solid fa-chevron-right"></i></a>',
-    responsive: [
-      // 반응형 웹 구현 옵션
-      {
-        breakpoint: 1441, //화면 사이즈 960px
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 830,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 481,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
   });
 
   $(window).scroll(function () {
